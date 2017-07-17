d4354f6dfa77eb8361b4e35c26e2ace480ff6c19
przbadu <pushparaj.badu@gmail.com>
2017-07-17 17:25:26 +0545

    Add bootstrap support

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './HelloWorld/components/HelloWorld';
 import registerServiceWorker from './registerServiceWorker';
+
+// Styles
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+// components
+import App from './HelloWorld/components/HelloWorld';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
